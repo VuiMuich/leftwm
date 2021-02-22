320557765bc5a8d10a68460218d3f55a8f590f7b
VuiMuich <jm.spam@gmx.net>
2021-02-22 20:13:43 +0100

    set visibility by focus (incomplete)

--- src/layouts/monocle.rs
+++ src/layouts/monocle.rs
@@ -1,8 +1,8 @@
 use crate::models::Window;
 use crate::models::Workspace;
-// use crate::models::WindowState;
+use crate::models::Manager;
 
-/// Layout which gives only one window with the full desktop realestate. A monocle mode.
+/// Layout which gives only one window in a monocle mode.
 pub fn update(workspace: &Workspace, windows: &mut Vec<&mut &mut Window>) {
     let window_count = windows.len();
 
@@ -12,29 +12,17 @@ pub fn update(workspace: &Workspace, windows: &mut Vec<&mut &mut Window>) {
 
     let mut iter = windows.iter_mut();
 
-    //maximize primary window
-    {
-        if let Some(monowin) = iter.next() {
-            monowin.set_height(workspace.height());
-            monowin.set_width(workspace.width());
-            monowin.set_x(workspace.x());
-            monowin.set_y(workspace.y());
+    for w in iter {
+            w.set_height(workspace.height());
+            w.set_width(workspace.width());
+            w.set_x(workspace.x());
+            w.set_y(workspace.y());
 
-            monowin.set_visible(true);
-        }
+            let manager: Manager;
+            match manager.focused_window() {
+            w => w.set_visible(true),
+            _ => w.set_visible(false),
+            };
     }
-
-    //hide all other windows
-    {
-        if window_count > 1 {
-            for w in iter {
-                w.set_height(workspace.height());
-                w.set_width(workspace.width());
-                w.set_x(workspace.x());
-                w.set_y(workspace.y());
-
-                w.set_visible(false);
-            }
-        }
-    }
+    return;
 }
