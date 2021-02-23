01f8beaa07e2be669be4fa9979ee086967f33ad5
VuiMuich <jm.spam@gmx.net>
2021-02-23 01:34:49 +0100

    another attempt, still not working

--- src/layouts/monocle.rs
+++ src/layouts/monocle.rs
@@ -1,6 +1,5 @@
 use crate::models::Window;
 use crate::models::Workspace;
-use crate::models::Manager;
 
 /// Layout which gives only one window in a monocle mode.
 pub fn update(workspace: &Workspace, windows: &mut Vec<&mut &mut Window>) {
@@ -10,20 +9,19 @@ pub fn update(workspace: &Workspace, windows: &mut Vec<&mut &mut Window>) {
         return;
     }
 
-    let mut iter = windows.iter_mut();
+    let iter = windows.iter_mut();
 
     for w in iter {
             w.set_height(workspace.height());
             w.set_width(workspace.width());
             w.set_x(workspace.x());
             w.set_y(workspace.y());
+            w.set_visible(false);
 
-            let manager: Manager;
-            match manager.focused_window() {
-            w => w.set_visible(true),
-            //why is w losing scope here?
-            _ => w.set_visible(false),
+            let handle = DisplayEvent::FocusedWindow(handle, x, y);
+            if &w.focused_window_history[0].clone() == &w.handle  {
+                w.set_visible(true);
             };
+
     }
-    return;
 }
